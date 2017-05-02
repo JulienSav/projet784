31a60f79c1f07ab06403da82689bfcf7b9124484
juvari <julien.savarit@insa-cvl.fr>
2017-05-02 21:11:17 +0200

    test

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //connect to mongoose
-mongoose.connect('mongodb://localhost/projet');
-var db = mongoose.connection;
+//mongoose.connect('mongodb://localhost/projet');
+//var db = mongoose.connection;
 
 app.get('/', function(req, res){
 	res.send('Hello world !');
