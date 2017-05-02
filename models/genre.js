6c6d38206dfc133d8531c4b3f351da03595899c2
juvari <julien.savarit@insa-cvl.fr>
2017-05-02 21:37:50 +0200

    correctif

--- models/genre.js
+++ models/genre.js
@@ -9,10 +9,9 @@ var genreSchema = mongoose.Schema({
 		type: Date,
 		default:Date.now
 	}
-	
 });
 
-var Genre = module.exports = mongoose.Model('Genre', genreSchema);
+var Genre = module.exports = mongoose.model('Genre', genreSchema);
 
 module.exports.getGenres = function (callback, limit) {
 	Genre.find(callback).limit(limit);
