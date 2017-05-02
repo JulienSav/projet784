75d905c86ddda14cca5a725d2e4e72fce731a3bf
juvari <julien.savarit@insa-cvl.fr>
2017-05-02 21:09:40 +0200

    test

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/projet');
+var db = mongoose.connection;
+
+app.get('/', function(req, res){
+	res.send('Hello world !');
+});
+
+app.listen(8080);
+console.log('Running on port 8080');
